e5b40b7b6cf488b6047eb27c564df2c069991b34
heqyoufree <heqyou_free@126.com>
2019-01-23 19:08:12 +0800

    update

--- mitmproxy/fgo.py
+++ mitmproxy/fgo.py
@@ -1,97 +1,115 @@
+"""Import http module from mitmproxy."""
 from mitmproxy import http
 import json
 import base64
 import urllib
 import datetime
 
+
 def load(load):
-  load.add_option('block_global', bool, False, 'disable block_global')
-  #load.add_option('mode', str, 'socks5', 'set proxy mode')
+    """Load."""
+    load.add_option('block_global', bool, False, 'disable block_global')
+    # load.add_option('mode', str, 'socks5', 'set proxy mode')
+
 
 def response(flow: http.HTTPFlow) -> None:
-  if flow.request.url.find('ac.php') > -1 :
-    if flow.request.urlencoded_form['key'] == 'battlesetup' or flow.request.urlencoded_form['key'] == 'battleresume' :
-      json_data = json.loads(str(base64.b64decode(urllib.parse.unquote(str(flow.response.content, encoding = 'utf-8'))), encoding = 'utf-8'))
-      with open('./settings/' + json_data['cache']['replaced']['battle']['userId'] + '.json', 'r') as f:
-        options = json.loads(f.read())
-      if options['main'] :
-        if isset(json_data['cache']['replaced']['battle']):
-          print(datetime.datetime.now().strftime('%H:%M:%S') + '-' + json_data['cache']['replaced']['battle']['userId'] + 'start battle')
-          for sv in json_data['cache']['replaced']['battle'][0]['battleInfo']['userSvt']:
-            if isset(sv['hpGaugeType']):
-              if options['enemyActNumSwitch']:
-                sv['maxActNum'] = options['enemyActNumTo']
-              if options['enemyChargeTurnSwitch']:
-                sv['chargeTurn'] = options['enemyChargeTurnto']
-              continue
-            if isset(sv['status']) and isset(sv['userId']) and sv['userId'] != '0' and sv['userId'] != 0:
-              if isinstance(sv['hp'],int):
-                sv['hp'] = int(sv['hp']) * options['uHp']
-              else:
-                sv['hp'] = str(int(sv['hp']) * options['uHp'])
+    """Replace the data of response."""
+    if flow.request.url.find('ac.php') > -1:
+        if flow.request.urlencoded_form['key'] == 'battlesetup' or flow.request.urlencoded_form['key'] == 'battleresume':
+            json_data = json.loads(str(base64.b64decode(urllib.parse.unquote(str(flow.response.content, encoding='utf-8'))), encoding='utf-8'))
+            with open('./settings/' + json_data['cache']['replaced']['battle']['userId'] + '.json', 'r') as f:
+                options = json.loads(f.read())
+            if options['main']:
+                if isset(json_data['cache']['replaced']['battle']):
+                    print(datetime.datetime.now().strftime('%H:%M:%S') + '-' + json_data['cache']['replaced']['battle']['userId'] + 'start battle')
+                    for sv in json_data['cache']['replaced']['battle'][0]['battleInfo']['userSvt']:
+                        if isset(sv['hpGaugeType']):
+                            if options['enemyActNumSwitch']:
+                                sv['maxActNum'] = options['enemyActNumTo']
+                            if options['enemyChargeTurnSwitch']:
+                                sv['chargeTurn'] = options['enemyChargeTurnto']
+                            continue
+                        if isset(sv['status']) and isset(sv['userId']) and sv['userId'] != '0' and sv['userId'] != 0:
+                            if isinstance(sv['hp'], int):
+                                sv['hp'] = int(sv['hp']) * options['uHp']
+                            else:
+                                sv['hp'] = str(int(sv['hp']) * options['uHp'])
 
-              if isinstance(sv['atk'],int):
-                sv['atk'] = int(sv['atk']) * options['uAtk']
-              else:
-                sv['atk'] = str(int(sv['atk']) * options['uAtk'])
-              
-              if options['skillLv']:
-                sv['skillLv1'] = 10
-                sv['skillLv2'] = 10
-                sv['skillLv3'] = 10
+                            if isinstance(sv['atk'], int):
+                                sv['atk'] = int(sv['atk']) * options['uAtk']
+                            else:
+                                sv['atk'] = str(int(sv['atk']) * options['uAtk'])
 
-              if options['tdLv']:
-                sv['treasureDeviceLv'] = 5
+                            if options['skillLv']:
+                                sv['skillLv1'] = 10
+                                sv['skillLv2'] = 10
+                                sv['skillLv3'] = 10
 
-              if options['limitCountSwitch']:
-                sv['limitCount'] = 4
-                sv['dispLimitCount'] = 4
-                sv['commandCardLimitCount'] = 3
-                sv['iconLimitCount'] = 4
+                            if options['tdLv']:
+                                sv['treasureDeviceLv'] = 5
 
-              if options['replaceSvtSwitch']:
-                if (options['replaceSvt1'] and sv['svtId'] == '600200') or options['replaceSvtSpinner'] == 1:
-                  replaceSvt(sv, 0)
-                if (options['replaceSvt2'] and sv['svtId'] == '600100') or options['replaceSvtSpinner'] == 2:
-                  replaceSvt(sv, 1)
-                if (options['replaceSvt3'] and sv['svtId'] == '601400') or options['replaceSvtSpinner'] == 3:
-                  replaceSvt(sv, 2)
-                if (options['replaceSvt4'] and sv['svtId'] == '700900') or options['replaceSvtSpinner'] == 4:
-                  replaceSvt(sv, 3)
-                if (options['replaceSvt5'] and sv['svtId'] == '700500') or options['replaceSvtSpinner'] == 5:
-                  replaceSvt(sv, 4)
-                if (options['replaceSvt6'] and sv['svtId'] == '701500') or options['replaceSvtSpinner'] == 6:
-                  replaceSvt(sv, 5)
-                  sv['treasureDeviceLv'] = 1
-                continue
+                            if options['limitCountSwitch']:
+                                sv['limitCount'] = 4
+                                sv['dispLimitCount'] = 4
+                                sv['commandCardLimitCount'] = 3
+                                sv['iconLimitCount'] = 4
 
-            if options['replaceCraftSwitch'] and isset(sv['parentSvtId']):
-              with open('./data.json', 'r') as f:
-                carftMap = json.loads(f.read())
-              sv['skillId1'] = carftMap['craft'][options['replaceCraftSpinner'] - 1]
+                            if options['replaceSvtSwitch']:
+                                if (options['replaceSvt1'] and sv['svtId'] == '600200') or options['replaceSvtSpinner'] == 1:
+                                    replaceSvt(sv, 0)
+                                if (options['replaceSvt2'] and sv['svtId'] == '600100') or options['replaceSvtSpinner'] == 2:
+                                    replaceSvt(sv, 1)
+                                if (options['replaceSvt3'] and sv['svtId'] == '601400') or options['replaceSvtSpinner'] == 3:
+                                    replaceSvt(sv, 2)
+                                if (options['replaceSvt4'] and sv['svtId'] == '700900') or options['replaceSvtSpinner'] == 4:
+                                    replaceSvt(sv, 3)
+                                if (options['replaceSvt5'] and sv['svtId'] == '700500') or options['replaceSvtSpinner'] == 5:
+                                    replaceSvt(sv, 4)
+                                if (options['replaceSvt6'] and sv['svtId'] == '701500') or options['replaceSvtSpinner'] == 6:
+                                    replaceSvt(sv, 5)
+                                    sv['treasureDeviceLv'] = 1
+                                continue
 
-        flow.response.content = bytes(urllib.parse.quote(str(base64.b64encode(bytes(json.dumps(json_data).replace(' ',''), encoding = 'utf-8')), encoding = 'utf-8')), encoding = 'utf-8')
+                        if options['replaceCraftSwitch'] and isset(sv['parentSvtId']):
+                            with open('./data.json', 'r') as f:
+                                carftMap = json.loads(f.read())
+                            sv['skillId1'] = carftMap['craft'][options['replaceCraftSpinner'] - 1]
+
+                flow.response.content = bytes(urllib.parse.quote(str(base64.b64encode(bytes(json.dumps(json_data).replace(' ', ''), encoding='utf-8')), encoding='utf-8')), encoding='utf-8')
+
 
 def isset(var):
-  try:
-    var
-  except NameError:
-    return False
-  else:
-    return True
+    """Check if the var is set.
+
+    Keyword arguments:
+    var -- the var that to be checked
+    """
+    try:
+        var
+    except NameError:
+        return False
+    else:
+        return True
+
 
 def replaceSvt(sv, id):
-  with open('./data.json', 'r') as f:
-    data = json.loads(f.read())
-  sv['svtId'] = data['svt'][id]['id']
-  sv['treasureDeviceId'] = data['svt'][id]['tdid']
-  sv['skillId1'] = data['svt'][id]['sk1']
-  sv['skillId2'] = data['svt'][id]['sk2']
-  sv['skillId3'] = data['svt'][id]['sk3']
-  sv['hp'] = data['svt'][id]['hp']
-  sv['atk'] = data['svt'][id]['atk']
-  if data['svt'][id]['limit']:
-    sv['limitCount'] = 0
-    sv['dispLimitCount'] = 0
-    sv['commandCardLimitCount'] = 0
-    sv['iconLimitCount'] = 0
+    """Replace the data of current svt.
+
+    Keyword arguments:
+    sv -- the reference var
+    id -- the id of the svt that is used to replace
+    """
+    with open('./data.json', 'r') as f:
+        data = json.loads(f.read())
+    sv['svtId'] = data['svt'][id]['id']
+    sv['treasureDeviceId'] = data['svt'][id]['tdid']
+    sv['skillId1'] = data['svt'][id]['sk1']
+    sv['skillId2'] = data['svt'][id]['sk2']
+    sv['skillId3'] = data['svt'][id]['sk3']
+    sv['hp'] = data['svt'][id]['hp']
+    sv['atk'] = data['svt'][id]['atk']
+    if data['svt'][id]['limit']:
+        sv['limitCount'] = 0
+        sv['dispLimitCount'] = 0
+        sv['commandCardLimitCount'] = 0
+        sv['iconLimitCount'] = 0
